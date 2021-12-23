c5dc5b2b1a747be65900e320c3361ea890fc9185
Mick Vleeshouwer <Mick.Vleeshouwer@microsoft.com>
2021-12-23 17:50:47 +0000

    First try for Light entity

---
+++ homeassistant/components/overkiz/light.py
@@ -0,0 +1,118 @@
+"""Support for Overkiz lights."""
+from __future__ import annotations
+
+from pyoverkiz.enums import OverkizCommand, OverkizCommandParam, OverkizState
+
+from homeassistant.components.light import (
+    ATTR_BRIGHTNESS,
+    ATTR_RGB_COLOR,
+    COLOR_MODE_BRIGHTNESS,
+    COLOR_MODE_ONOFF,
+    COLOR_MODE_RGB,
+    LightEntity,
+    brightness_supported,
+)
+from homeassistant.components.overkiz import HomeAssistantOverkizData
+from homeassistant.components.overkiz.coordinator import OverkizDataUpdateCoordinator
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+from .const import DOMAIN
+from .entity import OverkizEntity
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+):
+    """Set up the Overkiz lights from a config entry."""
+    data: HomeAssistantOverkizData = hass.data[DOMAIN][entry.entry_id]
+
+    entities = [
+        OverkizLight(device.device_url, data.coordinator)
+        for device in data.platforms[Platform.LIGHT]
+    ]
+
+    async_add_entities(entities)
+
+
+class OverkizLight(OverkizEntity, LightEntity):
+    """Representation of an Overkiz Light."""
+
+    def __init__(self, device_url: str, coordinator: OverkizDataUpdateCoordinator):
+        """Initialize a device."""
+        super().__init__(device_url, coordinator)
+
+        self._attr_supported_color_modes = set()
+
+        if self.executor.has_command(OverkizCommand.SET_RGB):
+            self._attr_supported_color_modes.add(COLOR_MODE_RGB)
+        if self.executor.has_command(OverkizCommand.SET_INTENSITY):
+            self._attr_supported_color_modes.add(COLOR_MODE_BRIGHTNESS)
+        if not self.supported_color_modes:
+            self._attr_supported_color_modes = {COLOR_MODE_ONOFF}
+
+    # @property
+    # def color_mode(self) -> str:
+    #     """Return the color mode of the light."""
+    #     if self.executor.has_command(OverkizCommand.SET_RGB):
+    #         return COLOR_MODE_RGB
+
+    #     if self.executor.has_command(OverkizCommand.SET_INTENSITY):
+    #         return COLOR_MODE_BRIGHTNESS
+
+    #     return COLOR_MODE_ONOFF
+
+    @property
+    def is_on(self) -> bool:
+        """Return true if light is on."""
+        return (
+            self.executor.select_state(OverkizState.CORE_ON_OFF)
+            == OverkizCommandParam.ON
+        )
+
+    @property
+    def rgb_color(self) -> tuple[int, int, int] | None:
+        """Return the rgb color value [int, int, int]."""
+        red = self.executor.select_state(OverkizState.CORE_RED_COLOR_INTENSITY)
+        green = self.executor.select_state(OverkizState.CORE_GREEN_COLOR_INTENSITY)
+        blue = self.executor.select_state(OverkizState.CORE_BLUE_COLOR_INTENSITY)
+
+        if None in [red, green, blue]:
+            return None
+
+        return (int(red), int(green), int(blue))
+
+    @property
+    def brightness(self) -> int | None:
+        """Return the brightness of this light between 0..255."""
+        if brightness := self.executor.select_state(OverkizState.CORE_LIGHT_INTENSITY):
+            return round(int(brightness) * 255 / 100)
+
+        return None
+
+    async def async_turn_on(self, **kwargs) -> None:
+        """Turn the light on."""
+        if ATTR_RGB_COLOR in kwargs and COLOR_MODE_RGB in self.supported_color_modes:
+            await self.executor.async_execute_command(
+                OverkizCommand.SET_RGB,
+                *[round(float(c)) for c in kwargs[ATTR_RGB_COLOR]],
+            )
+
+        if ATTR_BRIGHTNESS in kwargs and brightness_supported(
+            self.supported_color_modes
+        ):
+            brightness = round(float(kwargs[ATTR_BRIGHTNESS]) / 255 * 100)
+            await self.executor.async_execute_command(
+                OverkizCommand.SET_INTENSITY, brightness
+            )
+
+        else:
+            await self.executor.async_execute_command(OverkizCommand.ON)
+
+    async def async_turn_off(self, **_) -> None:
+        """Turn the light off."""
+        await self.executor.async_execute_command(OverkizCommand.OFF)
